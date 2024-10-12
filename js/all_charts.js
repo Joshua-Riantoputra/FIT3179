8aa724590a677614f020b456f73e48df23d1178e
Joshua-Riantoputra <jria0003@student.monash.edu>
2024-10-12 15:09:19 +1100

    its kinda looking alright

--- js/all_charts.js
+++ js/all_charts.js
@@ -3,5 +3,5 @@ vegaEmbed('#bubble_chart1', "js/bubble_chart.vg.json", {actions: false}).then(fu
 vegaEmbed('#bubble_chart2', "js/bubble_chart2.vg.json", {actions: false}).then(function(result) {    }).catch(console.error);
 vegaEmbed('#bubble_chart3', "js/bubble_chart3.vg.json", {actions: false}).then(function(result) {    }).catch(console.error);
 vegaEmbed('#heat', "js/table_heatmap.vg.json", {actions: false}).then(function(result) {}).catch(console.error);
-vegaEmbed('#bar', "js/stacked_bar.vg.json", {actions: false}).then(function(result) {}).catch(console.error);
+// vegaEmbed('#bar', "js/stacked_bar.vg.json", {actions: false}).then(function(result) {}).catch(console.error);
 vegaEmbed('#sankey', "js/example_sankey.vg.json", {actions: false}).then(function(result) {}).catch(console.error);
