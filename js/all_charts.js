eaaba458c3bb12bcaa255e9e8b670a973b0dfa4d
Joshua-Riantoputra <jria0003@student.monash.edu>
2024-10-12 14:35:28 +1100

    stability across zoom sizes, better bg colour

--- js/all_charts.js
+++ js/all_charts.js
@@ -1,7 +1,7 @@
-vegaEmbed('#choropleth_map', "js/choropleth_map.vg.json").then(function(result) {}).catch(console.error);
-vegaEmbed('#bubble_chart1', "js/bubble_chart.vg.json").then(function(result) {    }).catch(console.error);
-vegaEmbed('#bubble_chart2', "js/bubble_chart2.vg.json").then(function(result) {    }).catch(console.error);
-vegaEmbed('#bubble_chart3', "js/bubble_chart3.vg.json").then(function(result) {    }).catch(console.error);
-vegaEmbed('#heat', "js/table_heatmap.vg.json").then(function(result) {}).catch(console.error);
-vegaEmbed('#bar', "js/stacked_bar.vg.json").then(function(result) {}).catch(console.error);
-vegaEmbed('#sankey', "js/example_sankey.vg.json").then(function(result) {}).catch(console.error);
+vegaEmbed('#choropleth_map', "js/choropleth_map.vg.json", {actions: false}).then(function(result) {}).catch(console.error);
+vegaEmbed('#bubble_chart1', "js/bubble_chart.vg.json", {actions: false}).then(function(result) {    }).catch(console.error);
+vegaEmbed('#bubble_chart2', "js/bubble_chart2.vg.json", {actions: false}).then(function(result) {    }).catch(console.error);
+vegaEmbed('#bubble_chart3', "js/bubble_chart3.vg.json", {actions: false}).then(function(result) {    }).catch(console.error);
+vegaEmbed('#heat', "js/table_heatmap.vg.json", {actions: false}).then(function(result) {}).catch(console.error);
+vegaEmbed('#bar', "js/stacked_bar.vg.json", {actions: false}).then(function(result) {}).catch(console.error);
+vegaEmbed('#sankey', "js/example_sankey.vg.json", {actions: false}).then(function(result) {}).catch(console.error);
