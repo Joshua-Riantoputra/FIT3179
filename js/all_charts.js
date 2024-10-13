d0b5c1c36121563a7acada65510dc4b09d4ae90b
Joshua-Riantoputra <jria0003@student.monash.edu>
2024-10-13 12:20:43 +1100

    further changes

--- js/all_charts.js
+++ js/all_charts.js
@@ -3,5 +3,4 @@ vegaEmbed('#bubble_chart1', "js/bubble_chart.vg.json", {actions: false}).then(fu
 vegaEmbed('#bubble_chart2', "js/bubble_chart2.vg.json", {actions: false}).then(function(result) {    }).catch(console.error);
 vegaEmbed('#bubble_chart3', "js/bubble_chart3.vg.json", {actions: false}).then(function(result) {    }).catch(console.error);
 vegaEmbed('#heat', "js/table_heatmap.vg.json", {actions: false}).then(function(result) {}).catch(console.error);
-// vegaEmbed('#bar', "js/stacked_bar.vg.json", {actions: false}).then(function(result) {}).catch(console.error);
-vegaEmbed('#sankey', "js/example_sankey.vg.json", {renderer: 'svg', actions: false}).then(function(result) {}).catch(console.error);
+vegaEmbed('#sankey', "js/sankey.vg.json", {renderer: 'svg', actions: false}).then(function(result) {}).catch(console.error);
